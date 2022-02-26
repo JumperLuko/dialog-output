3fd80fbbc1dd2c4420503e9d203a2f310381b3ac
JumperLuko <jumper.luko@gmail.com>
2022-02-26 11:35:55 -0300

    ~ dialog-output the name was wrong, was dialog-ouput

--- INSTALL.sh
+++ INSTALL.sh
@@ -4,7 +4,7 @@
 if [ -e "/usr/bin/dialog" ] && [ -e "dialog-output.sh" ]; then
     dialog --backtitle "dialog-output installer" --yesno "\nDo you want to install dialog-output?!" 7 50
     if [ "$?" == 0 ]; then
-        sudo cp dialog-output.sh /usr/bin/dialog-ouput
+        sudo cp dialog-output.sh /usr/bin/dialog-output
         sudo chmod +x /usr/bin/dialog-ouput
         if [ -e "/usr/bin/dialog-ouput" ]; then
             dialog --backtitle "dialog-output installer" --title "Sucess!" --msgbox "\nDialog-output is installed!" 7 40

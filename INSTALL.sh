5087ba85c659916ac6473309a4190e1f0c758b3d
JumperLuko <jumper.luko@gmail.com>
2022-02-25 21:01:59 -0300

    + error <3

--- INSTALL.sh
+++ INSTALL.sh
@@ -6,12 +6,16 @@ if [ -e "/usr/bin/dialog" ] && [ -e "dialog-output.sh" ]; then
     if [ "$?" == 0 ]; then
         sudo cp dialog-output.sh /usr/bin/dialog-ouput
         sudo chmod +x /usr/bin/dialog-ouput
-        dialog --backtitle "dialog-output installer" --title "Sucess!" --msgbox "\nDialog-output is installed!" 7 40
+        if [ -e "/usr/bin/dialog-ouput" ]; then
+            dialog --backtitle "dialog-output installer" --title "Sucess!" --msgbox "\nDialog-output is installed!" 7 40
+        else
+            dialog --backtitle "dialog-output installer" --title "Error!" --msgbox "\nDialog-output installation failed!" 7 50
+        fi
     else
         dialog --backtitle "dialog-output installer" --title "Cancelled!" --msgbox "\nDialog-output installation was canceled!" 7 50
     fi
 elif [ -e "/usr/bin/dialog" ] && ! [ -e "dialog-output.sh" ]; then
-    dialog --backtitle "dialog-output installer" --title "Cancelled!" --msgbox "\nFailed to find dialog-output.sh" 7 40
+    dialog --backtitle "dialog-output installer" --title "Error!" --msgbox "\nFailed to find dialog-output.sh" 7 40
 elif ! [ -e "dialog-output.sh" ]; then
     echo -e "Failed to find dialog-output.sh"
 else
